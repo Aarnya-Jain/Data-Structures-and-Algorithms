5861779790ac884c1be4a3ee90878dd6d73c7df5
Aarnya Jain <aarnyajain417@gmail.com>
2025-09-04 02:09:18 +0530

    remove temp files

--- ds/stacks/lists/tempCodeRunnerFile.cpp
+++
@@ -1,8 +0,0 @@
-node * temp = top;
-    cout << endl;
-    cout << " -- " <<endl;
-    while(temp){
-      cout <<"| " <<temp->data << " |"<<endl;
-      temp=temp->next;
-    }
-    cout << " -- " <<endl;
